c3165ac9f27a51e34fc94297dc502f143cc3af43
Peter Yoon <peyo.hd@gmail.com>
2020-06-29 21:52:52 +0900

    update studio version

--- src/com/peyo/camtest/MainActivity.kt
+++ src/com/peyo/camtest/MainActivity.kt
@@ -3,40 +3,91 @@ package com.peyo.camtest
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.TextureView
-import android.view.ViewGroup
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraX
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewFinder: TextureView
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    val TAG = "CamTest"
+    private var preview: Preview? = null
+    private var camera: Camera? = null
+    lateinit var viewFinder: PreviewView
+
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
 	    super.onCreate(savedInstanceState)
 
-        viewFinder = TextureView(this)
+        viewFinder = PreviewView(this)
         setContentView(viewFinder)
 
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-            requestPermissions(arrayOf(Manifest.permission.CAMERA), 0)
-        else
-            viewFinder.post{ startCamera() }
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                    this, REQUIRED_PERMISSIONS, 0)
+        }
 
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults:
+            IntArray) {
+        if (requestCode == 0) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                        "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun startCamera() {
-        val config = PreviewConfig.Builder().build()
-        val preview = Preview(config)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        preview.setOnPreviewOutputUpdateListener {
-            val parent = viewFinder.parent as ViewGroup
-            parent.removeView(viewFinder)
-            parent.addView(viewFinder, 0)
-            viewFinder.surfaceTexture = it.surfaceTexture
-        }
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-        CameraX.bindToLifecycle(this, preview)
+            // Preview
+            preview = Preview.Builder()
+                    .build()
+
+            // Select back camera
+            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(
+                        this, cameraSelector, preview)
+                preview?.setSurfaceProvider(viewFinder.createSurfaceProvider())
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
